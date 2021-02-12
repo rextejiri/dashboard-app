4e8a3d8ba2eee0de7cc3d1efc46c2250a73bf54a
rextejiri <rextejiri@gmail.com>
2021-02-12 08:28:20 -0600

    all patients can be displayed on the dom

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,22 +1,50 @@
 import React from 'react';
+import axios from 'axios'
 import './App.css';
-import PatientList from './Patient/patientList.js'
-import PatientForm from './Patient/patientForm.js'
-import PatientSingle from './Patient/patientSingle.js'
+import PatientList from './Patient/PatientList.js'
+import PatientForm from './Patient/PatientForm.js'
+import PatientSingle from './Patient/PatientSingle.js'
 
 
 
 class App extends React.Component {
   constructor (props) {
     super(props);
+    this.state = {
+      patients: [],
+      currentPatient: {},
+    }
+    this.updateCurrentPatient = this.updateCurrentPatient.bind(this)
   }
+
+  componentDidMount (){
+    const url = 'http://localhost:3001/patient';
+
+    axios.get(url)
+    .then((response) => {
+      console.log(response);
+      this.setState({
+        patients: response.data
+      })
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+
+  updateCurrentPatient(updatedPatient){
+    this.setState({
+      currentPatient: updatedPatient
+    })
+  }
+
 
   render () {
     return (
       <div className="container">
-        <div> PatientList</div>
-        <div> PatientSingle</div>
-        <div> PatientForm</div>
+        <div><PatientList patients={this.state.patients} updatecurrentpatient={"this.updateCurrentPatient"}/></div>
+        <div> <PatientSingle /></div>
+        <div> <PatientForm /></div>
       </div>
     );
   }
--- frontend/src/Components/Patient/patientList.js
+++ frontend/src/Components/Patient/patientList.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import App from '../App.js'
 
-const PatientList = () => {
+
+
+const PatientList = (props) => {
  return(
-   <div>PatientList</div>
- )
+   <div>
+      <ul>
+        <li><h4>Patients</h4></li>
+        {props.patients.map((patient) => (
+          <div key={patient._id}>
+            {patient.firstName}{patient.lastName}
+          </div>
+        ))}
+      </ul>
+   </div>
+ );
 }
 
 export default PatientList;
